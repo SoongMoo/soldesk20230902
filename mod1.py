23c6fe95f8013baa9a50a27c5c5ce08da36fd00a
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2023-09-23 18:18:54 +0900

    Add files via upload

---
+++ mod1.py
@@ -0,0 +1,16 @@
+# mod1.py
+i = 10
+
+def add(num1, num2):
+	result = num1 + num2
+	return result
+
+PI = 3.141592
+
+class FourCal:
+	def add(self, num1, num2):
+		self.result = num1 + num2
+	def div(self, num1, num2):
+		self.result = num1 / num2   
+
+calc5 = FourCal()
